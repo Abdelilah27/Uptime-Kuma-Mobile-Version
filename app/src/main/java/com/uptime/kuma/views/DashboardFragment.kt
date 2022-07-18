2b9cabe0489bb97b1f23b63c1f06f3ae9f0bbba2
M'hammed Tabarani <tabarani_mhammed@emsi-edu.ma>
2022-07-18 17:39:50 +0100

    Login Screen

--- app/src/main/java/com/uptime/kuma/views/DashboardFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/DashboardFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uptime.kuma.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
