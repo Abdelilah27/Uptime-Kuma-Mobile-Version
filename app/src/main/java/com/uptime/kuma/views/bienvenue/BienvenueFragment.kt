6337f5be3ce049a8122e4e40dc5345641130f6ba
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-07-18 20:51:25 +0100

    Fix Portrait Mode - Button Navigation

--- app/src/main/java/com/uptime/kuma/views/bienvenue/BienvenueFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/bienvenue/BienvenueFragment.kt
@@ -6,17 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.uptime.kuma.R
+import com.uptime.kuma.databinding.FragmentBienvenueBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class BienvenueFragment : Fragment(R.layout.fragment_bienvenue) {
+
+    private var _binding: FragmentBienvenueBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         (activity as AppCompatActivity?)!!.getSupportActionBar()!!.hide()
-        return super.onCreateView(inflater, container, savedInstanceState)
+//        activity?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        _binding = FragmentBienvenueBinding.inflate(inflater, container, false)
+
+        binding.buttonCommencerBiennvenueFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_bienvenueFragment_to_loginFragment)
+        }
+
+        return binding.root
     }
+
+
 }
