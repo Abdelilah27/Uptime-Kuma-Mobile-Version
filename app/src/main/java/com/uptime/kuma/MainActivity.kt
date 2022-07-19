2d4ed308aad28a0bb4970e829eb5ab05ecf194f1
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-07-19 17:47:43 +0100

    Search Lister Fragment

--- app/src/main/java/com/uptime/kuma/MainActivity.kt
+++ app/src/main/java/com/uptime/kuma/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.uptime.kuma
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as
+                NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/uptime/kuma/views/monitorsList/AllServersFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/monitorsList/AllServersFragment.kt
@@ -1,6 +1,8 @@
 package com.uptime.kuma.views.monitorsList
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +14,7 @@ import com.uptime.kuma.models.MonitorItem
 class AllServersFragment : Fragment(R.layout.fragment_all_servers) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         val binding = FragmentAllServersBinding.bind(view)
         val itemAdapter = activity?.let { MonitorItemAllServersAdapter(it) }
         binding.apply {
@@ -20,15 +23,47 @@ class AllServersFragment : Fragment(R.layout.fragment_all_servers) {
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
                 itemAdapter?.submitList(getData())
+
             }
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_servers, menu)
+        val searchItem = menu.findItem(R.id.search_action)
+        val searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
+
+        searchView.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+
+            }
+
+        })
+
+    }
+
 
     private fun getData(): List<MonitorItem> {
         val data = arrayListOf<MonitorItem>()
         data.add(MonitorItem(id = 1, percent = "99.2%", title = "2M.ma", isOnline = false))
         data.add(MonitorItem(id = 1, percent = "100%", title = "Mobiblanc.ma", isOnline = true))
         data.add(MonitorItem(id = 1, percent = "72.99%", title = "Orange.ma", isOnline = true))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = true))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = true))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = true))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = true))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
+        data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = true))
         data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
         data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = false))
         data.add(MonitorItem(id = 1, percent = "87.29%", title = "Inwi.ma", isOnline = true))
