8c6361c357b3318ef6905b1a08dd5ff86f2cb555
boulami <boulami.hamza@gmail.comgit config --global user.email boulami.hamza@gmail.comgit config --global user.email boulami.hamza@gmail.com>
2022-07-21 19:08:02 +0200

    apparrence_fragment

---
+++ app/src/main/java/com/uptime/kuma/views/apparence/ApparenceReglageFragment.kt
@@ -0,0 +1,28 @@
+package com.uptime.kuma.views.apparence
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.uptime.kuma.R
+import com.uptime.kuma.databinding.FragmentApparenceReglageBinding
+
+class ApparenceReglageFragment : Fragment(R.layout.fragment_apparence_reglage) {
+
+    private lateinit var binding: FragmentApparenceReglageBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentApparenceReglageBinding.inflate(inflater,container,false)
+        val root: View =binding!!.root
+        val list=resources.getStringArray(R.array.list)
+        val arrayAdapter=ArrayAdapter(requireContext(),R.layout.list_item,list)
+        // Inflate the layout for this fragment
+        binding!!.auto.setAdapter(arrayAdapter)
+        return root
+    }
+
+}
