22fa086daeac90fb874033fa09e107b3f670f162
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-08-02 08:59:51 +0100

    Navigation Bottom Final

--- app/src/main/java/com/uptime/kuma/views/addStatus/AddStatusFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/addStatus/AddStatusFragment.kt
@@ -1,17 +1,13 @@
 package com.uptime.kuma.views.addStatus
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import com.uptime.kuma.MainActivity
 import com.uptime.kuma.R
 import com.uptime.kuma.databinding.FragmentAddStatusBinding
-import com.uptime.kuma.views.main.MainFragment
 
 
 class AddStatusFragment : Fragment(R.layout.fragment_add_status) {
@@ -26,18 +22,7 @@ class AddStatusFragment : Fragment(R.layout.fragment_add_status) {
     ): View? {
         _binding = FragmentAddStatusBinding.inflate(inflater, container, false)
         binding.addNewStatusButton.setOnClickListener {
-            //TODO
-            Log.d("A", "onCreateView: " + findNavController().currentDestination)
-            Log.d("B", "onCreateView: " + MainActivity.navController.currentDestination)
-            Log.d(
-                "C", "onCreateView: " + requireView().findNavController(
-                ).currentDestination
-            )
-            Log.d(
-                "D", "onCreateView: " + MainFragment.navController.currentDestination
-            )
-//            findNavController().navigateUp()
-//            MainFragment.navController.navigate(R.id.allServersFragment)
+            findNavController().navigateUp()
         }
         return binding.root
     }
