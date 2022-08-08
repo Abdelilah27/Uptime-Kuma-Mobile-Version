4f56c69e9e543715ef5ea676bd43fd5099c89769
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-08-08 09:34:30 +0100

    Switch getting data from main activity to splash fragment

--- app/src/main/java/com/uptime/kuma/views/apparence/ApparenceReglageFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/apparence/ApparenceReglageFragment.kt
@@ -1,11 +1,11 @@
 package com.uptime.kuma.views.apparence
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import com.uptime.kuma.R
 import com.uptime.kuma.databinding.FragmentApparenceReglageBinding
 
@@ -16,10 +16,10 @@ class ApparenceReglageFragment : Fragment(R.layout.fragment_apparence_reglage) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentApparenceReglageBinding.inflate(inflater,container,false)
-        val root: View =binding!!.root
-        val list=resources.getStringArray(R.array.list)
-        val arrayAdapter=ArrayAdapter(requireContext(),R.layout.list_item,list)
+        binding = FragmentApparenceReglageBinding.inflate(inflater, container, false)
+        val root: View = binding!!.root
+        val list = resources.getStringArray(R.array.list)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_item, list)
         // Inflate the layout for this fragment
         binding!!.auto.setAdapter(arrayAdapter)
         return root
--- app/src/main/java/com/uptime/kuma/views/splash/SplashFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/splash/SplashFragment.kt
@@ -5,18 +5,49 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.tinder.scarlet.Scarlet
 import com.uptime.kuma.R
+import com.uptime.kuma.api.ApiUtilities
+import com.uptime.kuma.api.ConnexionInterface
 import com.uptime.kuma.api.NetworkResult
 import com.uptime.kuma.databinding.FragmentSplashBinding
+import com.uptime.kuma.service.sharedData.SharedRepository
+import com.uptime.kuma.service.sharedData.SharedViewModel
+import com.uptime.kuma.service.sharedData.SharedViewModelFactory
 
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var scarlet: Scarlet
+        lateinit var webSocketService: ConnexionInterface
+        lateinit var sharedViewModel: SharedViewModel
+    }
+
+    private lateinit var sharedRepository: SharedRepository
 
     @SuppressLint("CheckResult")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSplashBinding.bind(view)
+
+        //Setup and create connexion
+        scarlet = ApiUtilities.provideScarlet(requireActivity().application)
+        webSocketService = ApiUtilities.getInstance(scarlet)
+
+        //Service Shared Data
+        sharedRepository = SharedRepository(webSocketService)
+        sharedViewModel = ViewModelProvider(
+            this,
+            SharedViewModelFactory(sharedRepository)
+        )[SharedViewModel::class.java]
+
+        sharedViewModel.handleConnexionState(this, lifecycleScope = lifecycleScope)
+
         binding.apply {
             //Direction to the Bienvenue fragment when the data are retrieved
             NetworkResult.instance.get().observe(viewLifecycleOwner, Observer {
