966a9bb970ae3f1d0303eb6bb3d4877359ebd56e
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-08-08 15:54:01 +0100

    add data to MutableLiveData

--- app/src/main/java/com/uptime/kuma/service/sharedData/SharedViewModel.kt
+++ app/src/main/java/com/uptime/kuma/service/sharedData/SharedViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.*
 import com.tinder.scarlet.WebSocket
 import com.uptime.kuma.api.NetworkResult
+import com.uptime.kuma.models.monitor.Monitor
 import com.uptime.kuma.utils.Constants
 import io.reactivex.Flowable
 import kotlinx.coroutines.CoroutineScope
@@ -13,9 +14,9 @@ import org.json.JSONObject
 
 class SharedViewModel(private val sharedRepository: SharedRepository) : ViewModel() {
 
-//    private val _monitorLiveData = MutableLiveData<List<Monitor>>()
-//    val monitorLiveData: LiveData<List<Monitor>>
-//        get() = _monitorLiveData
+    private val _monitorLiveData = MutableLiveData<Monitor>()
+    val monitorLiveData: LiveData<Monitor>
+        get() = _monitorLiveData
 
     //Get Data
     val data: Flowable<WebSocket.Event>
@@ -71,7 +72,61 @@ class SharedViewModel(private val sharedRepository: SharedRepository) : ViewMode
             for (i in 1 until jsonObject.length()) {
                 if (jsonObject.has(i.toString())) {
                     Log.d("REA", i.toString())
-                    Log.d("jsonObject1", jsonObject.getJSONObject(i.toString()).toString())
+                    //get separated jsonObjects
+                    val json = jsonObject.getJSONObject(i.toString())
+                    val name = json.get("name").toString()
+                    val id = json.get("id")
+                    val active = json.get("active")
+                    val dns_resolve_server = json.get("dns_resolve_server").toString()
+                    val dns_resolve_type = json.get("dns_resolve_type").toString()
+                    val expiryNotification = json.get("expiryNotification")
+                    val ignoreTls = json.get("ignoreTls")
+                    val interval = json.get("interval")
+                    val maxredirects = json.get("maxredirects")
+                    val maxretries = json.get("maxretries")
+                    val method = json.get("method").toString()
+                    val type = json.get("type").toString()
+                    val upsideDown = json.get("upsideDown")
+                    val url = json.get("url").toString()
+                    val weight = json.get("weight")
+                    val retryInterval = json.get("retryInterval")
+                    //accepted_statuscodes list
+                    var accepted_statuscodes: ArrayList<String> = ArrayList()
+                    val accepted_statuscodes_array =
+                        json.getJSONArray("accepted_statuscodes")
+                    for (j in 0 until accepted_statuscodes_array.length()) {
+                        val statusCode = accepted_statuscodes.add(
+                            accepted_statuscodes_array[j]
+                                .toString()
+                        )
+                        accepted_statuscodes.add(statusCode.toString())
+                    }
+                    //add data to MutableLiveData
+                    _monitorLiveData.postValue(
+                        Monitor(
+                            id = id as Int,
+                            name = name,
+                            active =
+                            active as Int,
+                            dns_resolve_server = dns_resolve_server,
+                            dns_resolve_type =
+                            dns_resolve_type,
+                            expiryNotification = expiryNotification as Boolean,
+                            ignoreTls = ignoreTls as Boolean,
+                            interval = interval as Int,
+                            maxredirects = maxredirects as Int,
+                            maxretries = maxretries as Int,
+                            method = method,
+                            type = type,
+                            upsideDown = upsideDown as Boolean,
+                            url = url,
+                            weight = weight as Int,
+                            retryInterval = retryInterval as Int,
+                            accepted_statuscodes = accepted_statuscodes
+                        )
+                    )
+
+                    Log.d("add", _monitorLiveData.value.toString())
                 } else {
                     continue
                 }
