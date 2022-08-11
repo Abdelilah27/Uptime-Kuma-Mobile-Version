f0e95ba105b2a7ba4b02f2c82fa1b4fab3b68916
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-08-11 11:25:44 +0100

    Item Adapter Fix

--- app/src/main/java/com/uptime/kuma/views/dashbord/DashboardFragment.kt
+++ app/src/main/java/com/uptime/kuma/views/dashbord/DashboardFragment.kt
@@ -4,29 +4,27 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.uptime.kuma.views.mainActivity.MainActivity
 import com.uptime.kuma.R
 import com.uptime.kuma.databinding.FragmentDashboardBinding
-import com.uptime.kuma.models.DashbordItems
 import com.uptime.kuma.views.adapters.DashboardRecyclerAdapter
-import com.uptime.kuma.views.allServers.AllServersCompanionObject
+import com.uptime.kuma.views.mainActivity.MainActivity
 
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard),
     DashboardRecyclerAdapter.OnItemClickListener {
-//    private lateinit var dashbordViewModel: DashbordViewModel
     private lateinit var itemAdapter: DashboardRecyclerAdapter
+
+    //    private lateinit var dashbordViewModel: DashbordViewModel
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentDashboardBinding.bind(view)
         // instantiation de ViewModel
 //        dashbordViewModel=ViewModelProvider(requireActivity()).get(DashbordViewModel::class.java)
-        val dashbordRecyclerAdapter = DashboardRecyclerAdapter(requireContext(), this)
         binding.apply {
             dashbordRecycler.apply {
-                adapter = dashbordRecyclerAdapter
+                itemAdapter = DashboardRecyclerAdapter(context, this@DashboardFragment)
+                adapter = itemAdapter
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
 //                dashbordRecyclerAdapter.submitList(getData())
