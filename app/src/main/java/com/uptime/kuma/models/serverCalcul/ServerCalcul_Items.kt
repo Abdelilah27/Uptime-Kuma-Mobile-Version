bb469a7db42e2519ee4d47a6ee71eb561dc72679
M'hammed Tabarani <tabarani_mhammed@emsi-edu.ma>
2022-08-19 12:33:02 +0100

    Calcule_status

---
+++ app/src/main/java/com/uptime/kuma/models/serverCalcul/ServerCalcul_Items.kt
@@ -0,0 +1,12 @@
+package com.uptime.kuma.models.serverCalcul
+
+data class ServerCalcul_Items(
+    val duration: Int?= null,
+    val id: Int?= null,
+    val important: Int?= null,
+    val monitor_id: Int?= null,
+    val msg: String?= null,
+    val ping: Int?= null,
+    val status: Int?= null,
+    val time: String ?= null
+)
